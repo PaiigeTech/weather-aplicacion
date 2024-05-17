5b613ba9d3cf275724cf2255c0e61acb6a881fdc
PaiigeTech <156714524+PaiigeTech@users.noreply.github.com>
2024-05-17 10:53:59 +0200

    Update app.js

--- apps.js
+++ apps.js
@@ -1,5 +1,5 @@
 function handleSearchSubmit(event){
-    event.prevent();
+    event.preventDefault();
     let searchInput=document.querySelector("#search-form-input");
     let cityElement=document.querySelector("#city");
     cityElement.innerHTML=searchInput.value
