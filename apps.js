6c49027de1196f5473392dfaffabb8768dbd14c2
PaiigeTech <156714524+PaiigeTech@users.noreply.github.com>
2024-05-18 23:49:25 +0200

    Injected the weather condition icon.

--- apps.js
+++ apps.js
@@ -6,8 +6,8 @@ function updateWeather(response){
  let humidityElement=document.querySelector("#humidity");
  let windSpeedElement=document.querySelector("#wind-speed");
  let timeElement=document.querySelector("#time");
- 
  let date= new Date(response.data.time * 1000);
+ let iconElement=document.querySelector("#icon");
 
  cityElement.innerHTML=response.data.city;
  humidityElement.innerHTML=`${response.data.temperature.humidity}`;
@@ -15,6 +15,8 @@ function updateWeather(response){
  windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
  timeElement.innerHTML=formatDate(date);
  temperatureElement.innerHTML=Math.round(temperature);
+ iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
 
 }
 
@@ -25,8 +27,8 @@ function formatDate(date){
     let days= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
     let day = days[date.getDay()];
 
- if (minutes<0){
-    minutes= `0${minutes}`
+ if (minutes < 10){
+    minutes= `0${minutes}`;
  }
  return `${day} ${hours} : ${minutes}`;
 }
